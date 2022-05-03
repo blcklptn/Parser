2cbc03ba88c4ce88c434b8bfaab43da2eb276685
Daniil Ermolaev <73552143+blcklptn@users.noreply.github.com>
2022-05-03 14:05:57 +0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+#  Список ссылок на группы
+LINKS = ['https://vk.com/linux0ids',
+         'https://vk.com/jpmovement',
+         'https://vk.com/torchfamily',
+         'https://vk.com/zxcgen',
+         'https://vk.com/absorballthepain'
+        ]
+    
+#  Основной код. Получаем данные из ссылки, а потом парсим их
+def parser(link: str="None") -> None:
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'lxml')
+    a = soup.find_all('a', href=True)
+    print("https://vk.com/"+a[0]['href'])
+
+
+#  Проходимся по всем ссылкам
+for link in LINKS:
+    parser(link)
